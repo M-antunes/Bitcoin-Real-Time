69a6352cfec4d5ab517d0caaad36a99b02152a91
Marcelo Antunes <mlsantunes@live.com>
2021-06-19 16:37:46 -0300

    Connetcted to an API

--- src/components/BtcPrice.vue
+++ src/components/BtcPrice.vue
@@ -2,39 +2,60 @@
   <img style="width: 200px" src="./../assets/Btc.png" />
   <h1>Bitcoin in Real Time</h1>
 
-  <div>
-    <h2>USD</h2>
-    <span>$36,000.00</span>
+  <div class="usd">
+    <h2 >USD</h2>
+    <span>${{ btcUsd }}</span>
   </div>
 
-  <div>
+  <div class='brl'>
     <h2>BRL</h2>
-    <span>R$194,000.00</span>
+    <span>${{ btcBrl }}</span>
   </div>
 
-  <button v-on:click="timer">{{time}}</button>
+  <div >
+   <h5>It'll respawn in: </h5>
+  <h3 class="time"> {{ time }} </h3>
+  </div>
 </template>
 
 <script>
 export default {
   name: "BtcPrice",
   data() {
-    return { time: 30 };
+    return {
+      time: 30,
+      btcBrl: 0,
+      btcUsd: 0,
+    };
   },
   methods: {
     timer() {
       setInterval(() => {
-          this.time--
-          if (this.time < 0) {
-              this.time = 30;
-          }
+        this.time--;
+        if (this.time < 0) {
+          this.time = 30;
+          this.fetchPrices();
+        }
       }, 1000);
     },
+
+    async fetchPrices() {
+      await fetch(`https://economia.awesomeapi.com.br/json/last/BTC-BRL,BTC-USD`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.btcBrl = data["BTCBRL"]["high"];
+          this.btcUsd = data["BTCUSD"]["high"];
+        })
+      .catch((error) => console.log(`Error to fetch data: ${error}`));
+    },
+  },
+  created() {
+    this.fetchPrices();
   },
   beforeMount() {
-      this.timer();
-  }
+    this.timer();
+  },
 };
-
-
 </script>
+
+
